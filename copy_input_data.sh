9773d2b4f4fb294081c307661bbf96f315e96365
anerv <vieroe@hotmail.com>
2025-06-26 15:16:48 +0200

    renamed config files

--- copy_input_data.sh
+++ copy_input_data.sh
@@ -9,7 +9,7 @@ fi
 provided_path="$1"
 
 # Task 1: Copy and rename config.yml to config-model.yml in the current directory
-cp "${provided_path}/config.yml" "./config-model.yml" || { echo "Failed to copy and rename config.yml"; exit 1; }
+cp "${provided_path}/config.yml" "./config_model.yml" || { echo "Failed to copy and rename config.yml"; exit 1; }
 
 # Task 2: Copy files ending with '_otp_geo.parquet' to current_directory/data/input
 cp "${provided_path}"/results/data/*_otp_geo.parquet data/input/ || { echo "No files matching '_otp_geo.parquet' found or failed to copy"; exit 1; }
